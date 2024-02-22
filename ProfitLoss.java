500b8ad75b89666038b75c896c1ea1330b723e5d
ashuy <ashuyad63@gmail.com>
2024-02-22 23:12:08 +0530

    Profit and Loss program

---
+++ ProfitLoss.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class ProfitLoss {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter the current price :");
+        int cp = sc.nextInt();
+        System.out.print("enter the selling price :");
+        int sp = sc.nextInt();
+        if(sp>cp){
+            System.out.println(sp-cp+" is the profit.");
+        }
+        if(sp<cp){
+            System.out.println(-(sp-cp)+" is the Loss.");
+        }
+
+    }
+}
