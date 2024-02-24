551274524bd91a44b00957d0a0837fb26cce740d
ashuy <ashuyad63@gmail.com>
2024-02-24 22:55:15 +0530

    traingle conditions  program

---
+++ triangle.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class triangle {
+    public static void main(String[] args){
+        Scanner read = new Scanner(System.in);
+        System.out.print("Enter your 1st side : ");
+        int a = read.nextInt();
+        System.out.print("Enter your 2nd side : ");
+        int b = read.nextInt();
+        System.out.print("Enter your 3rd side : ");
+        int c = read.nextInt();
+        if((a*a+b*b > c*c) || (a*a+c*c > b*b) || (b*b+c*c > a*a)){
+            System.out.println("this is a valid triangle.");
+
+        }
+        else{
+            System.out.println("This is not a valid triangle.");
+        }
+
+    }
+}
